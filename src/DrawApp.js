312999e744ff5f5b2d9e1e1f11fc71b1a79b909e
Ryan Rustyn <ryanrustyn@gmail.com>
2022-09-06 12:17:49 -0700

    add shuffle func

--- src/DrawApp.js
+++ src/DrawApp.js
@@ -16,9 +16,11 @@ function DrawApp() {
 
     const [cards, setCards] = useState([]);
 
-    //useEffect for initial deck
+    //an initial hook to get a deck to draw through
     useEffect(function fetchDeckOnMount() {
+        console.log("useEffect run")
         async function fetchDeck() {
+            console.log("Fetch deck run")
             const deckResult = await axios.get(`${BASE_URL}/new/shuffle/?deck_count=1`);
             setDeck({
                 deckId: deckResult.data.deck_id,
@@ -29,6 +31,8 @@ function DrawApp() {
         fetchDeck();
     }, []);
 
+    //make an axios request to draw a card, if no cards remaining display an
+    //alert error
     async function drawCard() {
         if (deck.cardsRemaining === 0) {
             alert('Error: no cards remaining!')
@@ -41,7 +45,28 @@ function DrawApp() {
             setCards([...cards, drawResult.data.cards[0]]);
         }
     }
-
+    
+    //make an API call to shuffle the deck
+    function shuffleDeck() {
+        setDeck({...deck, isLoading: true})
+        setCards([]);
+        
+        async function shuffle() {
+            const shuffleRes = await axios.get(`${BASE_URL}/${deck.deckId}/shuffle`);
+            setDeck({
+                ...deck,
+                cardsRemaining: shuffleRes.data.remaining,
+                isLoading: false,
+            });
+            
+            console.log("shuffle run")
+        }
+        
+        shuffle();
+    }
+    
+    console.log("state rendered line 68")
+    
     if (deck.isLoading) return <i>Loading...</i>
 
 
@@ -49,6 +74,7 @@ function DrawApp() {
     return (
         <div>
             <button onClick={drawCard}>Draw a card!</button>
+            <button onClick={shuffleDeck}>Shuffle the deck!</button>
             <div>
                 {cards.length !== 0 &&
                     cards.map(c => <Card card={c} key={c.code} />)}
